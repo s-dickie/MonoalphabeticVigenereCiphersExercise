a8279da95641ec54ba0102583757f51944ae94cf
Saeed Dickie <saeeddickie@gmail.com>
2018-12-19 21:15:53 +0000

    Initial implementation of monoalphabetic cipher encryption & decryption

---
+++ MonoalphabeticVigenereCiphersExercise/MonoCipher.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace MonoalphabeticVigenereCiphersExercise
+{
+    public class MonoCipher
+    {
+        static char[] alphabet = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        static char[] cipheralphabet = new char[] { 'L', 'E', 'O', 'P', 'A', 'R', 'D', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'Q', 'N', 'M', 'K', 'J', 'I', 'H', 'G', 'F', 'C', 'B' };
+
+
+
+        public void Encrypt()
+        {
+            string messagefile = "messageP";
+            string pathread = @"C:\\Users\Saeed Dickie\source\repos\MonoalphabeticVigenereCiphersExercise\MonoalphabeticVigenereCiphersExercise\CipherFiles\" + messagefile + ".txt";
+            string pathwrite = @"C:\\Users\Saeed Dickie\source\repos\MonoalphabeticVigenereCiphersExercise\MonoalphabeticVigenereCiphersExercise\CipherFiles\messageC.txt";
+
+
+            byte[] TextCharArray = File.ReadAllBytes(pathread);
+
+            ArrayList CipherCharOutput = new ArrayList();
+            string output = "";
+
+
+            foreach (var character in TextCharArray)
+            {
+               if (character == 32) CipherCharOutput.Add(Convert.ToChar(" "));
+                if (character == 13) CipherCharOutput.Add(Convert.ToChar("\r"));
+                if (character == 10) CipherCharOutput.Add(Convert.ToChar("\n"));
+
+
+                for (int i = 0; i < alphabet.Length; i++)
+                {
+                    if (character == Convert.ToByte(alphabet[i]))
+                    {
+                        var cipherletter = Convert.ToChar(cipheralphabet[i]);
+
+                        CipherCharOutput.Add(cipherletter);
+                    }
+                }
+                
+            }
+
+                       
+            StreamWriter sw = new StreamWriter(pathwrite);
+
+            foreach (var item in CipherCharOutput)
+            {
+                output += item;
+            
+            }
+
+            sw.WriteLine(output);
+
+            sw.Close();
+        }
+
+
+
+  
+
+        public void Decrypt()
+        {
+        string messagefile = "messageC";
+        string pathread = @"C:\\Users\[]\source\repos\MonoalphabeticVigenereCiphersExercise\MonoalphabeticVigenereCiphersExercise\CipherFiles\" + messagefile + ".txt";
+        string pathwrite = @"C:\\Users\[]\source\repos\MonoalphabeticVigenereCiphersExercise\MonoalphabeticVigenereCiphersExercise\CipherFiles\messageD.txt";
+
+
+         byte[] TextCharArray = File.ReadAllBytes(pathread);
+
+         ArrayList CipherCharOutput = new ArrayList();
+        string output = "";
+
+
+        foreach (var character in TextCharArray)
+        {
+            if (character == 32) CipherCharOutput.Add(Convert.ToChar(" "));
+            if (character == 13) CipherCharOutput.Add(Convert.ToChar("\r"));
+            if (character == 10) CipherCharOutput.Add(Convert.ToChar("\n"));
+
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (character == Convert.ToByte(cipheralphabet[i]))
+                {
+                    var decryptletter = Convert.ToChar(alphabet[i]);
+
+                    CipherCharOutput.Add(decryptletter);
+                }
+            }
+
+        }
+
+
+        StreamWriter sw = new StreamWriter(pathwrite);
+
+        foreach (var item in CipherCharOutput)
+        {
+            output += item;
+
+        }
+
+        sw.WriteLine(output);
+
+        sw.Close();
+    }
+
+    }
+
+}
+
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("117735b5-44f3-4b80-af44-0ba8d3548906")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
